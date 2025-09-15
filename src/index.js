639346f2770266a0bef14cb8bb4796e5b13cf9b6
icpinto <36252590+icpinto@users.noreply.github.com>
2025-09-15 13:45:39 +0100

    fix: prevent websocket double-close in dev

--- src/index.js
+++ src/index.js
@@ -7,11 +7,9 @@ import ThemeContext from './context/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <ThemeContext>
-      <App />
-    </ThemeContext>
-  </React.StrictMode>
+  <ThemeContext>
+    <App />
+  </ThemeContext>
 );
 
 // If you want to start measuring performance in your app, pass a function
