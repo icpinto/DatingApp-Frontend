dcf796a8b1351b2ee1c26cbabe9a7a20ef006296
icpinto <icpinto76.ic@gmail.com>
2025-10-04 13:44:25 +0100

    dffd

--- src/services/matchPreferences.js
+++ src/services/matchPreferences.js
@@ -1,6 +1,6 @@
 import api from "./api";
 
-const basePath = "/core-preferences";
+const basePath = "/user/core-preferences";
 
 const buildAuthHeaders = () => {
   const token = localStorage.getItem("token");
